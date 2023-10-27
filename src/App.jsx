73affc426087f6bd840336eeca79edb576106b36
bautistaRenaudo <bautista.renaudo@ivelopment.com>
2023-10-27 16:55:17 -0300

    mqtt form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { MQTTForm } from './components/MQTTForm'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>MQTT React App</h1>
+      <MQTTForm />
+    </>
+  )
+}
+
+export default App
